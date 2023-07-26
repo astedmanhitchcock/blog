6577a3086235df4cc594b7d8f454e252308f7d44
Aaron Hitchcock <emailclubaaron@gmail.com>
2023-07-25 23:08:09 -0400

    hoist state to store, adds missing migration and updates nav.

--- web/src/components/BaseNav.vue
+++ web/src/components/BaseNav.vue
@@ -1,29 +1,32 @@
 <template>
-  <nav class="base-nav container p-4 flex items-center justify-between">
-    <h1>
-      <LogoLink />
-    </h1>
-    <div>
-      <div class="base-nav__menu" :class="{['base-nav__menu--open']: isMobileMenuOpen}">
-        <router-link
-          v-for="(link, index) in links"
-          :key="link.path"
-          :to="link.path"
-          class="relative text-rose-quartz / py-3 md:px-2 md:py-1"
-          :class="{
-            'border-t md:border-l md:border-t-0': index > 0
-          }"
-        >
-          {{ link.label }}
-        </router-link>
+  <nav class="base-nav">
+    <div class="relative bg-base-bg w-full md:w-auto z-10">
+      <div class="base-nav__header">
+        <h1>
+          <LogoLink />
+        </h1>
+        <MobileToggle class="md:hidden" :menu-ref="$refs.menu" />
       </div>
-      <MobileToggle class="md:hidden" @change="handleToggleChange" />
+    </div>
+    <div ref="menu" class="base-nav__menu" :class="{['base-nav__menu--open']: isMobileNavOpen }">
+      <router-link
+        v-for="(link, index) in links"
+        :key="link.path"
+        :to="link.path"
+        class="relative text-rose-quartz / py-3 md:px-2 md:py-1"
+        :class="{
+          'border-t md:border-l md:border-t-0': index > 0
+        }"
+      >
+        {{ link.label }}
+      </router-link>
     </div>
   </nav>
 </template>
 
 <script>
-import LogoLink from './LogoLink.vue';
+import { store } from '@/store.js';
+import LogoLink from '@/components/LogoLink.vue';
 import MobileToggle from '@/components/BaseNavMobileToggle.vue'
 
 export default {
@@ -34,7 +37,6 @@ export default {
   },
   data() {
     return {
-      isMobileMenuOpen: false,
       links: [
       {
           'label': 'cv',
@@ -47,12 +49,14 @@ export default {
       ]
     }
   },
+  computed: {
+    isMobileNavOpen() {
+      return store.isMobileNavOpen
+    }
+  },
   methods: {
-    handleClickAway() {
-
-    },
-    handleToggleChange(isOpen) {
-      this.isMobileMenuOpen = isOpen
+    handleLinkClick() {
+      store.isMobileNavOpen = false
     }
   }
 }
@@ -60,40 +64,32 @@ export default {
 
 <style lang="scss" scoped>
 .base-nav {
-  @apply bg-base-bg;
-  position: relative;
-  z-index: 1;
+  @apply relative w-full flex items-center justify-between bg-base-bg mx-auto max-w-screen-md lg:max-w-screen-lg;
+
+  &__header {
+    @apply mx-auto p-4 flex justify-between items-center w-full max-w-screen-sm md:max-w-screen-md;
+  }
 
   &__menu {
-    @apply absolute w-full left-0 top-full flex flex-col bg-white px-4 
-      md:relative md:w-auto md:left-auto md:top-auto md:flex-row md:bg-transparent md:px-4;
-    transform: translateY(-100%);
-    opacity: 0;
-    z-index: -1;
-    transition: transform 0.35s ease, opacity 0.35s ease;
+    @apply absolute w-full left-0 top-full flex flex-col bg-white px-4
+      md:relative md:w-auto md:left-auto md:top-auto md:flex-row md:bg-transparent md:px-4 md:drop-shadow-none;
+    width: 100vw;
+    left: 50%;
+    transform: translate(-50%, -100%);
+    transition: transform 0.35s ease;
 
-    &:before {
-      @apply absolute top-0;
-      content: '';
-      display: block;
-      height: 100%;
-      width: 200vw;
-    }
-
-    &:before {
-      @apply bg-white drop-shadow-xl;
-      left: -50vw;
-    }
-    
     &--open {
-      transform: translateY(0);
-      opacity: 1;
+      @apply drop-shadow-lg;
+      transform: translate(-50%, 0);
     }
 
     @media (min-width: 768px) {
-      transform: translateY(0);
+      transform: translate(0);
       opacity: 1;
       z-index: 0;
+      width: auto;
+      left: auto;
+      filter: drop-shadow(0 0 #0000);
 
       &:after,
       &:before {
